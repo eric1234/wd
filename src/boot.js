0a032909d25b4acd83dae610a8711e48fd3a695c
Eric Anderson <eric@pixelwareinc.com>
2017-05-11 10:25:30 -0400

    Refactor tray controller
    The inheritance was entirely unnecessary. In fact we don't even need
    a class. Just exposing a function is sufficient.
    
    Also moved the actual launching of the tray on ready to the boot script
    rather than having it be a side effect of including the tray module.

--- src/boot.js
+++ src/boot.js
@@ -1,5 +1,7 @@
-import './controllers/tray'
-
 import { app } from 'electron'
+
+import tray from './controllers/tray'
+app.on('ready', tray)
+
 import { event_store } from './models/event_store'
 app.on('quit', () => event_store.insert_idle())
--- src/controllers/tray.js
+++ src/controllers/tray.js
@@ -1,18 +1,17 @@
-import { app, Menu, Tray } from 'electron'
+import { Menu, Tray } from 'electron'
 import { prompt } from './prompt'
 import Report from './report'
 
-class _Tray extends Tray {
-  constructor() {
-    super(`${__dirname}/../icon.png`)
-    this.on('click', () => prompt.ask())
-    this.setContextMenu(Menu.buildFromTemplate([
-      { label: 'Update', click: () => prompt.ask() },
-      { label: 'Report', click: () => new Report() },
-      { label: 'Exit', role: 'quit' },
-    ]))
-  }
+/*
+ * Construct a tray icon which then become the launch point for the rest of the
+ * application. Expected to only be called once during boot.
+ */
+export default function() {
+  let tray = new Tray(`${__dirname}/../icon.png`)
+  tray.on('click', () => prompt.ask())
+  tray.setContextMenu(Menu.buildFromTemplate([
+    { label: 'Update', click: () => prompt.ask() },
+    { label: 'Report', click: () => new Report() },
+    { label: 'Exit', role: 'quit' },
+  ]))
 }
-
-export let tray
-app.on('ready', () => tray = new _Tray())

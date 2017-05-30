978487fd105bccfeacf15cd9921e41acb2cdf2ae
Eric Anderson <eric@pixelwareinc.com>
2017-05-30 13:53:04 -0400

    Package support for Windows
    With these changes we can now generate an installable package for
    Windows. Once installed it will have a shortcut with the right name,
    logo, etc.
    
    There is some roughness with this that are documented on issue #4.

--- src/boot.js
+++ src/boot.js
@@ -1,5 +1,7 @@
+import { app } from 'electron'
+if( require('electron-squirrel-startup') ) app.quit();
+
 import './controllers/tray'
 
-import { app } from 'electron'
 import { event_store } from './models/event_store'
 app.on('quit', () => event_store.insert_idle())
